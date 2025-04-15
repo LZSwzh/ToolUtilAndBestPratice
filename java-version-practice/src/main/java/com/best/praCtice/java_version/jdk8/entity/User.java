5d8700491e379122e395e0d2ddf98fa3757332c2
wangzh4 <wangzh4@novastar.tech>
2025-04-15 14:27:21 +0800

    feat(version):新增JDK9新特性(模块化)

--- java-version-practice/src/main/java/com/best/praCtice/java_version/jdk8/entity/User.java
+++ java-version-practice/src/main/java/com/best/praCtice/java_version/jdk8/entity/User.java
@@ -1,16 +1,44 @@
-package com.best.praCtice.java_version.jdk8.entity;
+package com.best.practice.java_version.jdk8.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
 public class User {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Double getCount() {
+        return count;
+    }
+
+    public void setCount(Double count) {
+        this.count = count;
+    }
+
     private String name;
 
     private Integer age;
 
     private Double count;
+
+    public User() {
+
+    }
+
+    public User(String name, Integer age, Double count) {
+        this.name = name;
+        this.age = age;
+        this.count = count;
+    }
 }
