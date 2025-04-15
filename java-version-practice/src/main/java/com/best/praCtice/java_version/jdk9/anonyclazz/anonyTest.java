f4949af5699882ac0c2eb4f23f03bd1cce6e51c6
wangzh4 <wangzh4@novastar.tech>
2025-04-15 14:35:04 +0800

    fix(java-version):删除异常包

--- java-version-practice/src/main/java/com/best/praCtice/java_version/jdk9/anonyclazz/anonyTest.java
+++
@@ -1,7 +0,0 @@
-package com.best.practice.java_version.jdk9.anonyclazz;
-
-public class anonyTest {
-    public static void main(String[] args) {
-
-    }
-}
--- java-version-practice/src/main/java/com/best/praCtice/java_version/jdk9/jpms/tmp.java
+++
@@ -1,7 +0,0 @@
-package com.best.practice.java_version.jdk9.jpms;
-
-public class tmp {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
