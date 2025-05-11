31c0d59cb132ec3af660d90ed1e3172d960e6834
wzh <18403909715@163.com>
2025-05-11 13:40:09 +0800

    feat(java-version):新增函数式接口的API联系
    - 新增Consumer\supplier\function等函数式接口的API

--- java-version-practice/src/main/java/com/best/practice/java_version/jdk8/bisic/funcInterface/ConsumerFun.java
+++ java-version-practice/src/main/java/com/best/practice/java_version/jdk8/bisic/funcInterface/ConsumerFun.java
@@ -4,6 +4,12 @@ import java.util.function.Consumer;
 
 public class ConsumerFun {
     public static void main(String[] args) {
-
+        Consumer<String> myConsumer1 = (t) -> System.out.println("myConsumer1："+t);
+        //消费者接口的使用accept
+        myConsumer1.accept("hello accept");
+        System.out.println("-------------------------------------------------------");
+        //and的使用
+        Consumer<String> myConsumer2 = myConsumer1.andThen((t -> System.out.println("myConsumer2：" + t)));
+        myConsumer2.accept("hello andThen");
     }
 }
